9842e6e58e626ef153c1acd3f5d3ee6d8b7ed0d2
atilileri <atil@live.it>
2025-02-15 21:16:23 +0100

    solve 01-14 custom components

--- 06-01-ReactFacts/index.jsx
+++ 06-01-ReactFacts/index.jsx
@@ -1,31 +1,28 @@
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
 
-We'll be adding more styling to it later.
-
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="react-logo.png"` in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
+/**
+ * Challenge (part 1):
+ * Create a custom "Page" component
+ * 
+ * It should return an ordered list with the reasons why you're
+ * excited to be learning React :)
+ * 
+ * Render the Page component.
  */
-  import { createRoot } from "react-dom/client"
-  const root = createRoot(document.getElementById("root"))
-  
-  root.render(
-      <main>
-        <img width="120px" src="react-logo.png" alt="React logo" />
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
+
+root.render(
+    <Page />
+)
+
+function Page() {
+  return (
+    <ol>
+      <li>Chance to return my ideas into working projects</li>
+      <li>Earn passive income</li>
+      <li>Play around with web apps</li>
+      <li>Gateway to mobile apps</li>
+      <li>Job opportunities</li>
+    </ol>
   )
-  
+}
