74b0ba51cd2aac5628bc1d936debad89b5b35794
atilileri <atil@live.it>
2025-02-15 21:41:11 +0100

    add nested components

--- 06-01-ReactFacts/index.jsx
+++ 06-01-ReactFacts/index.jsx
@@ -28,24 +28,37 @@ function Header() {
     </header>
   )
 }
+
+function MainContent() {
+  return (
+    <main>
+    <h1>React Facts!</h1>
+    <ol>
+      <li>Chance to return my ideas into working projects</li>
+      <li>Earn passive income</li>
+      <li>Play around with web apps</li>
+      <li>Gateway to mobile apps</li>
+      <li>Job opportunities</li>
+    </ol>
+  </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2025 Ilerialkan development. All rights reserved.</small>
+    </footer>
+  )
+}
+
 function Page() {
   return (
     // <> </> inserts Fragment element implicitly
     <>
       <Header/>
-      <main>
-        <h1>React Facts!</h1>
-        <ol>
-          <li>Chance to return my ideas into working projects</li>
-          <li>Earn passive income</li>
-          <li>Play around with web apps</li>
-          <li>Gateway to mobile apps</li>
-          <li>Job opportunities</li>
-        </ol>
-      </main>
-      <footer>
-        <small>© 2025 Ilerialkan development. All rights reserved.</small>
-      </footer>
+      <MainContent/>
+      <Footer/>
     </>
   )
 }
