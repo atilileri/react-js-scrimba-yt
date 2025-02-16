daea0f52c56efc23ef7c34d5b28378a2b2871d24
atilileri <atil@live.it>
2025-02-16 16:19:57 +0100

    add solve 01.26 background logo

--- 07-01-ReactFacts-v2/components/Navbar.jsx
+++ 07-01-ReactFacts-v2/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="react-logo.png" alt="react-logo" />
+                <img src="/images/react-logo.png" alt="react-logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
