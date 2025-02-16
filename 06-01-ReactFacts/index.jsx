a0927dd6b88b1ffffe0d34c7efe2ee323a8a1876
atilileri <atil@live.it>
2025-02-16 14:08:20 +0100

    add solve 01-19 styling with classes

--- 06-01-ReactFacts/index.jsx
+++ 06-01-ReactFacts/index.jsx
@@ -2,19 +2,10 @@ import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
 /**
-Challenge: 
-
-Part 2: 
-- Add a `<header>` element with an `<img />` element with the image of the 
-  React logo inside (src="react-logo.png") and make sure to set the 
-  width to something more manageable so it doesn't take up the whole screen.
-  Also, as always, you should include some alt text on the image.
-- Add an `<h1>` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list, then wrap
-  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
-  structure flowing well.
-- Add a `<footer>` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
+ * Challenge:
+ * 
+ * - Add a `nav` > `ul` > `li` (x3). The 3 items should say:
+ *   "Pricing", "About", and "Contact"
  */
 
 root.render(
@@ -25,6 +16,13 @@ function Header() {
   return (
     <header>
       <img src="react-logo.png" alt="React logo" width="100px"/>
+      <nav>
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   )
 }
