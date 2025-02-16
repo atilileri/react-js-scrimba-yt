df60f23c80d53e79e2fcba5779fcc6f6e1ec934e
atilileri <atil@live.it>
2025-02-16 15:52:20 +0100

    add solve 01.24 navbar

--- 07-01-ReactFacts-v2/components/Navbar.jsx
+++ 07-01-ReactFacts-v2/components/Navbar.jsx
@@ -1,5 +1,21 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <header>
+            <nav>
+                <img src="react-logo.png" alt="react-logo" />
+                <span>React Facts!</span>
+            </nav>
+        </header>
     )
 }
